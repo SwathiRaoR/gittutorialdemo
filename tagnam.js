692a8e4f0595574e7b5cbb88d4864dc815e835d2
Swathi.RaoR <swati.raor@gmail.com>
2022-09-26 00:13:13 +0530

    addingtagname

---
+++ tagnam.js
@@ -0,0 +1,16 @@
+//var li=document.getElementsByTagName('li');
+//console.log(li);
+//console.log(li[1]);
+//items[1].textContent="hello2";
+//items[1].style.fontWeight='bold';
+//li[2].style.backgroundColor='green';
+
+var cl1=document.getElementsByClassName("list-item");
+var li1=document.getElementsByTagName('li');
+console.log(li1[5]);
+for(var i=0;i<li1.length;i++){
+   //li1[i].style.fontWeight='bold';
+   //li1[i].style.backgroundColor='green';
+   cl1[i].style.backgroundColor='yellow';
+    
+}
